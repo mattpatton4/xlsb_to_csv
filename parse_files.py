d9d95ee469cb14d909c4df4dabf2318265fc5667
bryant00 <bryantpatton@gmail.com>
2020-07-31 11:10:51 -0700

    notes

--- parse_files.py
+++ parse_files.py
@@ -1,15 +1,23 @@
 from pyxlsb import open_workbook, convert_date
 import csv
 
+""" 
+filename to read from. can be setup to read multiple files.
+"""
 f = "Solar+Battery Example v12d (PPA+Bat).xlsb"
-f = "testfile.xlsb"
-output_file = "combined"
+# f = "testfile.xlsb"
+# output_file = "combined"
+
+"""
+filters the tabs to read by tab name, i.e. "PaR Inputs"
+"""
 tabsFilter = "inputs"
 sheets = [s for s in open_workbook(f).sheets if s.lower().find(tabsFilter) != -1]
 
 with open_workbook(f) as wb:
     for sheetname in sheets:
         with wb.get_sheet(sheetname) as sheet:
+            # creates a csv for each tab in notebook with that tab's data
             with open(sheetname + ".csv", "w") as f:
                 writer = csv.writer(f)
                 for row in sheet.rows():
